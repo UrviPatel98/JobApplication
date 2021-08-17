d60dcc6cfdb537428c2527b8f0c47dc8d037e6b6
Urvi Thummar <urvithummar@Urvis-MacBook-Pro.local>
2021-08-17 17:29:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/jobapplication/ResultdetailActivity.kt
@@ -0,0 +1,64 @@
+package com.example.jobapplication
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import com.example.androidtraining.helper.IS_LOGIN
+import com.example.androidtraining.helper.PreferenceHelper.get
+import com.example.jobapplication.fragment.HomeFragment
+
+class ResultdetailActivity : BaseActivity() {
+
+    lateinit var txtemp: TextView
+    lateinit var txtexam: TextView
+    lateinit var txtuni: TextView
+    lateinit var txtlocation: TextView
+    lateinit var btnrview: Button
+    lateinit var btnback: ImageButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultdetail) 
+
+        txtemp=findViewById(R.id.txtemp)
+        txtexam=findViewById(R.id.txtexam)
+        txtuni=findViewById(R.id.txtuni)
+        txtlocation=findViewById(R.id.txtlocation)
+        btnrview=findViewById(R.id.btnrview)
+        btnback=findViewById(R.id.btnback)
+
+        initView()
+    }
+
+    private fun initView() {
+
+            btnrview.setOnClickListener {
+
+                if (prif[IS_LOGIN]!!) {
+                    val uri: Uri = Uri.parse(intent.getStringExtra("LINK"))
+
+                    val intent = Intent(Intent.ACTION_VIEW, uri)
+                    startActivity(intent)
+                }else
+                {
+                    Toast.makeText(this, getString(R.string.lbl_you_r_not_logged_in_what_to_login), Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+            txtemp.text=intent.getStringExtra("URL")
+            txtexam.text=intent.getStringExtra("EXAM")
+            txtuni.text=intent.getStringExtra("UNIVERSITY")
+            txtlocation.text=intent.getStringExtra("LOCATION")
+
+        btnback.setOnClickListener {
+            finish()
+
+        }
+    }
+}
